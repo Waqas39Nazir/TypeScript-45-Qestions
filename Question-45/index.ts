0df0d77cc4b90fc72338a890596e93fbd7394205
Waqas Nazir <waqasnazir@altaurux.com>
2024-03-24 21:00:57 +0500

    Q45 Done

---
+++ Question-45/index.ts
@@ -0,0 +1,37 @@
+/****
+ * Cars: Write a function that stores information about a car in a Object.
+ *  The function should always receive a manufacturer and a model name.
+ *  It should then accept an arbitrary number of keyword arguments.
+ * Call the function with the required information and two other name-value pairs,
+ *  such as a color or an optional feature. Print the Object that’s returned
+ * to make sure all the information was stored correctly.
+ */
+
+interface car {
+  manufacturer: string;
+  modelName: string;
+  color: string;
+  price?: string;
+}
+
+const make_car = (
+  manufacturer: string,
+  modelName: string,
+  color: string,
+  price?: string
+): car => {
+  return {
+    manufacturer,
+    modelName,
+    color,
+    price,
+  };
+};
+
+const carOne = make_car("VicyTech", "GH123TY", "Green");
+
+console.log("Car One:", carOne);
+
+const carTwo = make_car("Tesla", "GHGYTUY677687HGHJG", "Orange", "USD 12M");
+
+console.log("Car Two:", carTwo);
