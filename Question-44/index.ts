38af0583633e960b3a00c5d46f6c1d2fd117b94d
Waqas Nazir <waqasnazir@altaurux.com>
2024-03-24 21:20:07 +0500

    Q44 Done

---
+++ Question-44/index.ts
@@ -0,0 +1,21 @@
+/****
+ * Sandwiches: Write a function that accepts a array of items a
+ *  person wants on a sandwich. The function should have one parameter
+ *  that collects as many items as the function call provides, and it
+ *  should print a summary of the sandwich that is being ordered.
+ * Call the function three times, using a different number of arguments each time.
+ */
+
+const orderedSandwich = (...items: string[]): void => {
+  const message = `The ordered Sandwich has items${items.map((item) => {
+    return ` ${item}`;
+  })}.`;
+
+  console.log(message);
+};
+
+orderedSandwich("Ham", "Cheese", "Tomato");
+
+orderedSandwich("Swiss Cheese", "Mustard");
+
+orderedSandwich("Swiss Cheese", "Mustard", "Jam", "Jelly", "Ham");
