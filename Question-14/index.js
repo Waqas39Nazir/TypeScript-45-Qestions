caf80e72c4168ae15cff1711267827d14f5a004d
Waqas Nazir <waqasnazir@altaurux.com>
2024-03-24 22:07:36 +0500

    Q-14 Done

--- Question-14/index.js
+++ Question-14/index.js
@@ -1,14 +1,11 @@
+"use strict";
 /****
  * Guest List: If you could invite anyone, living or deceased, to dinner,
  *  who would you invite? Make a list that includes at least three people
  *  you’d like to invite to dinner. Then use your list to print a message to each person,
  *  inviting them to dinner.
  */
-
 const guests = ["Waqas", "Waqar", "Hassan", "Ahsan"];
-
-guests.forEach((guest: string) => {
-  console.log(
-    `Hi Mr.${guest}, Please come to my home tomorrow for a dinner at 18:00`
-  );
+guests.forEach((guest) => {
+    console.log(`Hi Mr.${guest}, Please come to my home tomorrow for a dinner at 18:00`);
 });
