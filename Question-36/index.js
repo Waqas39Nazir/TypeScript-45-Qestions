122728dcca1794d62a16ade615b50fbd3db10aa0
Waqas Nazir <waqasnazir@altaurux.com>
2024-03-24 22:51:54 +0500

    Q36 Done

---
+++ Question-36/index.js
@@ -0,0 +1,13 @@
+"use strict";
+/***
+ * T-Shirt: Write a function called make_shirt() that accepts a size and the text
+ *  of a message that should be printed on the shirt. The function should print
+ * a sentence summarizing the size of the shirt and the message printed on it.
+ *  Call the function.
+ */
+const make_shirt = (size, message) => {
+    console.log(`The shirt with size ${size} has the message \"${message}\" printed on it.`);
+};
+make_shirt("XL", "Be the best");
+make_shirt("XXL", "I am Back");
+make_shirt("SM", "Life your life without any fear");
---
+++ Question-36/index.ts
@@ -0,0 +1,16 @@
+/***
+ * T-Shirt: Write a function called make_shirt() that accepts a size and the text
+ *  of a message that should be printed on the shirt. The function should print
+ * a sentence summarizing the size of the shirt and the message printed on it.
+ *  Call the function.
+ */
+
+const make_shirt = (size: string, message: string): void => {
+  console.log(
+    `The shirt with size ${size} has the message \"${message}\" printed on it.`
+  );
+};
+
+make_shirt("XL", "Be the best");
+make_shirt("XXL", "I am Back");
+make_shirt("SM", "Life your life without any fear");
