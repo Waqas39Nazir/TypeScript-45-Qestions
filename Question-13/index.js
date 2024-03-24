00169795e7b6739b0380013351c7b68041005bce
Waqas Nazir <waqasnazir@altaurux.com>
2024-03-24 22:03:09 +0500

    Q13 Done

--- Question-13/index.js
+++ Question-13/index.js
@@ -1,12 +1,11 @@
+"use strict";
 /***
  * Q13: Your Own Array: Think of your favorite mode of transportation,
  *  such as a motorcycle or a car, and make a list that stores several examples.
  *  Use your list to print a series of statements about these items,
  *  such as “I would like to own a Honda motorcycle.”
  */
-
 const motorcycles = ["CD 70", "Honda 125", "Raod Prince"];
-
 motorcycles.forEach((motorcycle) => {
-  console.log(`I would like to buy a ${motorcycle}`);
+    console.log(`I would like to buy a ${motorcycle}`);
 });
