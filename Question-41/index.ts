04525b207491d7f1bd003742c62b67975481ca01
Waqas Nazir <waqasnazir@altaurux.com>
2024-03-24 19:56:19 +0500

    Q41 Done

--- Question-41/index.ts
+++ Question-41/index.ts
@@ -1,5 +1,5 @@
 /****
- * Magicians: Make a array of magician’s names. Pass the array
+ * Q41: Magicians: Make a array of magician’s names. Pass the array
  *  to a function called show_magicians(), which prints the name
  *  of each magician in the array.
  */
