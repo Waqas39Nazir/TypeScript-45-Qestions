4eb48cc042607140df50e38516d0c8a39547bb89
Waqas Nazir <waqasnazir@altaurux.com>
2024-04-07 07:05:07 +0500

    Q20 Done

--- Question-20/index.js
+++ Question-20/index.js
@@ -1,19 +1,18 @@
+"use strict";
 /***
  * Think of something you could store in a array. For example, you could make a list of mountains,
  *  rivers, countries, cities, languages, or anything else you’d like.
  * Write a program that creates a list containing these items.
  */
-
-const storedArray: (string | number)[] = [
-  "K2",
-  "Turkey",
-  "Saudi Arabia",
-  "Iran",
-  "Urdu",
-  "Sraiki",
-  1,
-  5,
-  8,
+const storedArray = [
+    "K2",
+    "Turkey",
+    "Saudi Arabia",
+    "Iran",
+    "Urdu",
+    "Sraiki",
+    1,
+    5,
+    8,
 ];
-
 console.log("My elements in array are of mixed datatypes:", storedArray);
