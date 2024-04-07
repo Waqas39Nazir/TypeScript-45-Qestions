ffe8bb8663161c094c278a14e2611da01800ad35
Waqas Nazir <waqasnazir@altaurux.com>
2024-04-07 06:20:48 +0500

    Q19 Done

--- Question-19/index.js
+++ Question-19/index.js
@@ -1,10 +1,8 @@
+"use strict";
 /****
  * Dinner Guests: Working with one of the programs from Exercises 14 through 18,
  *  print a message indicating the number of people you are inviting to dinner.
  */
-
-const guests: string[] = ["Waqas", "Nazir", "Hassan", "Waqar", "Ahsan"];
-
+const guests = ["Waqas", "Nazir", "Hassan", "Waqar", "Ahsan"];
 const numberOfGuests = guests.length;
-
 console.log(`Total guests invited at dinner:`, numberOfGuests);
